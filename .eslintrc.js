a18ba11ea3d7be152b8b21abe5a8b76c28569099
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-10-28 12:13:03 -0300

    add babelrc and add parameters parser babel eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     jest: true,
   },
   extends: ['airbnb-base', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
