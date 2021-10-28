c469e47c8f7259c9f2cbac6d795ed695a0ebc813
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-10-28 12:14:53 -0300

    add test create user

--- src/app/repositories/fakes/FakeUserRepository.js
+++ src/app/repositories/fakes/FakeUserRepository.js
@@ -1,16 +1,45 @@
 import { v4 as uuid } from 'uuid';
 
 class FakeUserRepository {
-  async create(name, email, password) {
-    const users = [];
+  constructor() {
+    this.users = [];
+  }
 
+  async findByEmail(email) {
+    const checkUser = this.users.find((user) => user.email === email);
+
+    return checkUser;
+  }
+
+  async findById(id) {
+    const checkUser = this.users.find((user) => user.id === id);
+
+    return checkUser;
+  }
+
+  async update({ id, name, password }) {
+    const checkUser = this.users.findIndex((user) => user.id === id);
+
+    const user = { name, password };
+
+    this.users[checkUser] = { id, name: user?.name, password: user?.password };
+
+    return this.users[checkUser];
+  }
+
+  async create(name, email, password) {
     const user = { name, email, password };
 
-    Object.assign(user, { id: uuid(), name, email, password });
+    const userWithId = Object.assign(user, {
+      id: uuid(),
+      name,
+      email,
+      password,
+    });
 
-    users.push(user);
+    this.users.push(user);
 
-    return users;
+    return userWithId;
   }
 }
 
